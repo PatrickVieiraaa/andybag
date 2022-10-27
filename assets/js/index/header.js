cd5d27b4bb0f2eb367d6d03a6f4253636b278c2f
Patrick Vieira Lima <patrickvieira.dev@gmail.com>
2022-10-27 09:36:56 -0300

    Initial commit

---
+++ assets/js/index/header.js
@@ -0,0 +1,13 @@
+function menuOpened() {
+   
+    let menuArea = document.getElementById("menu-area");
+
+    if (menuArea.style.width == '400px') {
+      menuArea.style.width == '0px'
+    } else {
+
+      menuArea.style.width == '200px'
+    }
+  }
+
+
