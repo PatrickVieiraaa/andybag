2dcb1ae0c1ac3e8573c127763f1560a4b107df17
Patrick Vieira Lima <patrickvieira.dev@gmail.com>
2022-10-31 14:02:58 -0300

    update menu mobile
    adicionado menu mobile responsivo

--- assets/js/index/nav.js
+++ assets/js/index/nav.js
@@ -1,37 +1,9 @@
-(function (){
+function menuabrir() {
+  let menuArea = document.getElementById("menu-area");
 
-  // Variáveis
-
-  const nav = document.querySelector('nav')
-  const menu = nav.querySelector('div[name="menu"]')
-  const botaoMenu = nav.querySelector('button[name="abrirMenu"]')
-
-  // Coloca classe no navbar quando ele é scrollado
-
-  function adicionarClasse () {
-    window.addEventListener('scroll', () => {
-      const alturaPagina = window.scrollY
-      if (alturaPagina < 10) nav.classList.remove('scroll')
-      else nav.classList.add('scroll')
-    })
+  if (menuArea.style.width == "200px") {
+    menuArea.style.width = "0px";
+  } else {
+    menuArea.style.width = "200px";
   }
-
-  // Abre o navbar clicando no botão de menu MOBILE
-
-  function abrirMenu () {
-    nav.addEventListener('click', (elemento) => {
-      const abrirMenu = 'button[name="abrirMenu"]'
-      if (!elemento.target.matches(abrirMenu)) return
-      
-      const menuAberto = menu.classList.contains('mostrar')
-      if (menuAberto) menu.classList.remove('mostrar'), botaoMenu.classList.remove('ativo')
-      else menu.classList.add('mostrar'), botaoMenu.classList.add('ativo')
-    })
-  }
-
-  // Ativar tudo
-  adicionarClasse()
-  abrirMenu()
-
-}())
-
+}
